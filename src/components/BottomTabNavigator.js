8cbfeb2e55af40f45e09bc5734718dafccf802cf
RobinTHOREL <thorelrobin@yahoo.fr>
2021-01-28 23:06:01 +0100

    :100: fkin bar

--- src/components/BottomTabNavigator.js
+++ src/components/BottomTabNavigator.js
@@ -5,20 +5,52 @@ import TrackListScreen from '../screens/TrackListScreen';
 import TrackDetailScreen from '../screens/TrackDetailScreen';
 import TrackCreateScreen from '../screens/TrackCreateScreen';
 import AccountScreen from '../screens/AccountScreen';
+import { StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons';
+
+const styles = StyleSheet.create({
+    tabsStyle: {
+        backgroundColor: '#303F9F'
+    }
+})
 
 export default createMaterialBottomTabNavigator(
     {
-        trackListFlow: createStackNavigator({
-            TrackList: TrackListScreen,
-            TrackDetail: TrackDetailScreen,
-        }),
-        TrackCreate: TrackCreateScreen,
-        Account: AccountScreen
+        trackListFlow: {
+            screen: createStackNavigator({
+                TrackList: TrackListScreen,
+                TrackDetail: TrackDetailScreen,
+            }),
+            navigationOptions: {
+                tabBarIcon: ({ focused, tintColor }) => {
+                    const iconName = `car${focused ? '' : '-outline'}`;
+                    return <Ionicons name={iconName} size={25} color={tintColor} />;
+                },
+            }
+        },
+        TrackCreate: {
+            screen: TrackCreateScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => {
+                    return <MaterialCommunityIcons name='magnify' size={25} color={tintColor} />;
+                },
+            }
+        },
+        Account: {
+            screen: AccountScreen,
+            navigationOptions: {
+                tabBarIcon: ({ focused, tintColor }) => {
+                    const iconName = `account-circle${focused ? '' : '-outline'}`;
+                    return <MaterialCommunityIcons name={iconName} size={25} color={tintColor} />;
+                },
+            }
+        }
     },
     {
         initialRouteName: 'Account',
-        activeColor: '#f0edf6',
-        inactiveColor: '#3e2465',
-        barStyle: { backgroundColor: '#694fad' },
+        activeColor: '#e8eaf6',
+        inactiveColor: '#c5cae9',
+        barStyle: styles.tabsStyle,
     }
 );
