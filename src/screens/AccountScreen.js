84bd3dae5d76d61549b08d843453671275590f36
Youyou <ydiouri@eleven-labs.com>
2021-02-03 22:24:11 +0100

    #7: add Formik and Yup - add new fields

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -8,23 +8,23 @@ import { Context as AuthContext } from '../context/AuthContext';
 const AccountScreen = () => {
     const { signout } = useContext(AuthContext);
     return <SafeAreaView forceInset={{ top: 'always' }}>
-        <Spacer></Spacer>
+        <Spacer> </Spacer>
             <Text style={{ fontSize: 22 }}>Soog - Mon compte</Text>
             <Spacer>
-                <Button 
-                title="Deconnexion" 
-                buttonStyle={styles.button}  
-                onPress={signout} 
+                <Button
+                title="Deconnexion"
+                buttonStyle={styles.button}
+                onPress={signout}
                 />
             </Spacer>
         </SafeAreaView>;
 };
 const styles = StyleSheet.create({
-    button: {  
+    button: {
        backgroundColor: '#dc143c',
        borderColor: '#f08080',
        borderWidth: 5,
-       borderRadius: 25    
+       borderRadius: 25
     }
  });
 
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,30 +1,172 @@
-import React, { useContext } from 'react';
-import { ImageBackground, View, StyleSheet } from 'react-native';
-import { NavigationEvents } from 'react-navigation';
+import React, { useContext, useRef } from 'react';
+import { ImageBackground, ScrollView, StyleSheet } from 'react-native';
+import { NavigationEvents, SafeAreaView } from 'react-navigation';
+import { useFormik } from 'formik';
+import { Button, Input, Text } from 'react-native-elements';
 import { Context as AuthContext } from '../context/AuthContext';
-import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
+import Spacer from '../components/Spaces';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+    firstName: Yup.string()
+        .min(2, 'Trop court!')
+        .max(50, 'Trop long!')
+        .required('Requis'),
+    lastName: Yup.string()
+        .min(2, 'Trop court!')
+        .max(50, 'Trop long!')
+        .required('Requis'),
+    email: Yup.string().email('Email non valide').required('Requis'),
+    phoneNumber: Yup.string()
+        .matches(
+            /(\+212|0)([ \-_/]*)(\d[ \-_/]*){9}/,
+            {
+                message: 'Format incorrect (formats acceptés: +212XXXXXXXXX / XXXXXXXXXX'
+            }
+        )
+        .required('Requis'),
+    password: Yup.string()
+        .min(6, 'Trop court!')
+        .required('Requis'),
+    passwordCheck: Yup.string()
+        .min(6, 'Trop court!')
+        .required('Requis')
+        .test('passwords-match', 'Passwords must match ya fool', function(value) {
+            return this.parent.password === value;
+        }),
+});
 
 const SignUpScreen = () => {
-    const { state, signup, clearErrorMessage } = useContext(AuthContext);
+    const lastName = useRef(null);
+    const email = useRef(null);
+    const phoneNumber = useRef(null);
+    const password = useRef(null);
+    const { state, clearErrorMessage } = useContext(AuthContext);
+    const {
+        handleChange,
+        handleSubmit,
+        handleBlur,
+        errors,
+        touched
+    } = useFormik({
+        validationSchema: SignupSchema,
+        initialValues: { firstName: '', lastName: '', email: '', phoneNumber: '', password: '', passwordCheck: '' },
+        onSubmit: (values) => alert(`Email : ${values.email}`) });
 
     return (
-        <View style={styles.container}>
-            <ImageBackground source={require('../img/logo_bg_sign.png')} style={styles.background}>
-            </ImageBackground>
+    <ScrollView style={styles.container}>
+        <SafeAreaView forceInset={{ top: 'always' }}>
+        </SafeAreaView>
+        <ImageBackground source={require('../img/logo_bg_sign.png')} style={styles.background}>
+        </ImageBackground>
 
-            <NavigationEvents onWillFocus={clearErrorMessage} />
-            <AuthForm
-            errorMessage={state.errorMessage}
-            submitButtonText="S'inscrire"
-            onSubmit={signup}
+        <NavigationEvents onWillFocus={clearErrorMessage} />
+            <Input
+                label="Prénom"
+                onChangeText={handleChange('firstName')}
+                autoCorrect={false}
+                returnKeyType='next'
+                returnKeyLabel='next'
+                onBlur={handleBlur('email')}
+                errorStyle={{ color: 'red' }}
+                errorMessage={errors.firstName}
+                inputContainerStyle={{ borderColor: errors.firstName ?'red' : 'blue' }}
+                touched={touched.firstName}
+                onSubmitEditing={() => lastName.current?.focus()}
             />
-            <NavLink
-                routeName="Signin"
-                text="Vous avez déjà un compte? Connectez-vous."
+            <Input
+                label="Nom"
+                onChangeText={handleChange('lastName')}
+                autoCorrect={false}
+                returnKeyType='next'
+                returnKeyLabel='next'
+                errorStyle={{ color: 'red' }}
+                errorMessage={errors.lastName}
+                inputContainerStyle={{ borderColor: errors.lastName ?'red' : 'blue' }}
+                touched={touched.lastName}
+                onSubmitEditing={() => email.current?.focus()}
+                ref={lastName}
             />
-                
-        </View>
+            <Input
+                label="Email"
+                onChangeText={handleChange('email')}
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoCompleteType='email'
+                returnKeyType='next'
+                returnKeyLabel='next'
+                onBlur={handleBlur('email')}
+                errorStyle={{ color: 'red' }}
+                errorMessage={errors.email}
+                inputContainerStyle={{ borderColor: errors.email ?'red' : 'blue' }}
+                touched={touched.email}
+                onSubmitEditing={() => phoneNumber.current?.focus()}
+                ref={email}
+            />
+            <Input
+                label="Téléphone"
+                onChangeText={handleChange('phoneNumber')}
+                autoCapitalize="none"
+                autoCorrect={false}
+                returnKeyType='next'
+                returnKeyLabel='next'
+                onBlur={handleBlur('phoneNumber')}
+                errorStyle={{ color: 'red' }}
+                errorMessage={errors.phoneNumber}
+                inputContainerStyle={{ borderColor: errors.phoneNumber ?'red' : 'blue' }}
+                touched={touched.phoneNumber}
+                onSubmitEditing={() => password.current?.focus()}
+                ref={phoneNumber}
+
+            />
+        <Spacer>
+            <Input
+                secureTextEntry
+                label="Mot de passe"
+                onChangeText={handleChange('password')}
+                autoCapitalize="none"
+                autoCorrect={false}
+                returnKeyType='next'
+                returnKeyLabel='next'
+                onBlur={handleBlur('password')}
+                errorStyle={{ color: 'red' }}
+                errorMessage={errors.password}
+                inputContainerStyle={{ borderColor: errors.password ?'red' : 'blue' }}
+                touched={touched.password}
+                ref={password}
+            />
+        </Spacer>
+        <Spacer>
+            <Input
+                secureTextEntry
+                label="Confirmer votre mot de passe"
+                onChangeText={handleChange('passwordCheck')}
+                autoCapitalize="none"
+                autoCorrect={false}
+                returnKeyType='go'
+                returnKeyLabel='go'
+                onBlur={handleBlur('passwordCheck')}
+                errorStyle={{ color: 'red' }}
+                errorMessage={errors.passwordCheck}
+                inputContainerStyle={{ borderColor: errors.passwordCheck ?'red' : 'blue' }}
+                touched={touched.passwordCheck}
+                onSubmitEditing={() => handleSubmit()}
+            />
+        </Spacer>
+        {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
+        <Spacer>
+            <Button
+                title="S'inscrire"
+                buttonStyle={styles.button}
+                onPress={handleSubmit}
+            />
+        </Spacer>
+        <NavLink
+            routeName="Signin"
+            text="Vous avez déjà un compte? Connectez-vous."
+        />
+    </ScrollView>
     );
 };
 
@@ -36,12 +178,24 @@ SignUpScreen.navigationOptions = () => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center'
+        flex: 1
     },
     background: {
         ...StyleSheet.absoluteFillObject
-}
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: 'red',
+        marginLeft: 15
+    },
+    button: {
+        backgroundColor: '#303F9F',
+        borderColor: '#303F9F',
+        borderWidth: 5,
+        borderRadius: 25,
+        justifyContent: 'center',
+        width: 400,
+    }
 });
 
 export default SignUpScreen;
